65d97ef8096a6df53d6752d3f4bd0fea67816ba2
JACOB BRETHERTON <1800231@uad.ac.uk>
2020-02-01 12:10:33 +0000

    Player conroller added

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+   // [SerializeField]
+   // GameObject audioManagerObject;
+   // AudioManagerScript audioManager;
+
+    [SerializeField]
+    private float playerSpeed = 10;
+    [SerializeField]
+    private float jumpVelocity = 20;
+    [SerializeField]
+    private float airborneJumpAcceleration = 5;
+    [SerializeField]
+    private float horizontalSmoothingOnGround = 20;
+    [SerializeField]
+    private float horizontalSmoothingInAir = 5;
+    [SerializeField]
+    float initialCoyoteTime = 0.1f;
+
+    private Rigidbody2D playerRB;
+
+    public Vector2 directionOffset { get; private set; }
+
+    bool jumping;
+    bool jumpInitialPush;
+    bool jumpHeld;
+
+    bool onFloor;
+    bool onFloorLastTick;
+    [SerializeField]
+    float currentCoyoteTime = 0;
+
+    [SerializeField]
+    private GameObject player;
+
+
+    void Start()
+    {
+        playerRB = player.GetComponent<Rigidbody2D>();
+        //audioManager = audioManagerObject.GetComponent<AudioManagerScript>();
+        onFloorLastTick = true;
+    }
+
+
+
+    private void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+        {
+            jumpInitialPush = true;
+        }
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W))
+        {
+            jumpHeld = true;
+        }
+        
+    }
+
+
+
+    private void FixedUpdate()
+    {
+        Vector2 newVelocity = playerRB.velocity;
+
+        if(currentCoyoteTime > 0)
+        {
+            currentCoyoteTime -= Time.fixedDeltaTime;
+        }else if (currentCoyoteTime < 0)
+        {
+            currentCoyoteTime = 0;
+        }
+
+        if(Physics2D.Raycast(player.transform.position + new Vector3(-0.4f, 0, 0) + Vector3.down, Vector2.down*0.8f, 0) || Physics2D.Raycast(player.transform.position + new Vector3(+0.4f, 0, 0) + Vector3.down*0.8f, Vector2.down, 0)) // update: two rays now
+        {
+            onFloor = true;
+           
+            if((currentCoyoteTime < initialCoyoteTime) && (!jumpInitialPush || !jumpHeld))
+                jumping = false;
+
+            onFloorLastTick = true;
+        }
+        else
+        {
+            onFloor = false;
+        }
+
+      
+
+        if (onFloor == false && onFloorLastTick == true)
+        {
+            onFloorLastTick = false;
+            if (!jumping && (!jumpInitialPush||!jumpHeld))
+            {
+                currentCoyoteTime = initialCoyoteTime;
+            }
+            else
+            {
+                currentCoyoteTime = 0;
+            }
+        }
+
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        directionOffset = new Vector2(horizontalInput, 0);
+
+        if (onFloor) // jump from floor
+        {
+            newVelocity.x = Mathf.Lerp(playerRB.velocity.x, horizontalInput * playerSpeed, horizontalSmoothingOnGround * Time.fixedDeltaTime);
+
+            if(Input.GetAxis("Horizontal") ==0)
+            {
+                newVelocity.x = 0;
+            }
+
+            if (jumpInitialPush)
+            {
+                jumpInitialPush = false;
+                //if (Mathf.Abs(playerRB.velocity.y) < 15f)
+                //{
+                //    audioManager.PlayNextJump();
+                //}
+                newVelocity.y = jumpVelocity;
+                currentCoyoteTime = 0;
+                onFloor = false;
+                jumping = true;
+            }
+        }
+        else if(currentCoyoteTime > 0 && !onFloor && !jumping) // jump from coyote grace period
+        {
+            newVelocity.x = Mathf.Lerp(playerRB.velocity.x, horizontalInput * playerSpeed, horizontalSmoothingInAir * Time.fixedDeltaTime);
+            if (jumpInitialPush && playerRB.velocity.y <= 0)
+            {
+                jumpInitialPush = false;
+                //if (Mathf.Abs(playerRB.velocity.y) < 15f)
+                //{
+                //    audioManager.PlayNextJump();
+                //}
+                newVelocity.y = jumpVelocity;
+                currentCoyoteTime = 0;
+                onFloor = false;
+                jumping = true;
+            }
+            else if (jumpHeld || Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W)) // checks for both in case one is wrong, this is the best way i could think of                                                                             // although it doesn't acutually really matter if we miss a frame here
+            {
+                jumpHeld = false;
+                newVelocity.y += airborneJumpAcceleration * Time.fixedDeltaTime;
+            }
+        }
+        else // already in air
+        {
+            newVelocity.x = Mathf.Lerp(playerRB.velocity.x, horizontalInput * playerSpeed, horizontalSmoothingInAir * Time.fixedDeltaTime);
+
+            if (jumpHeld || Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W)) // checks for both in case one is wrong, this is the best way i could think of
+                                                                                    // although it doesn't acutually really matter if we miss a frame here
+            {
+                jumpHeld = false;
+                newVelocity.y += airborneJumpAcceleration * Time.fixedDeltaTime;
+            }
+        }
+
+        playerRB.velocity = newVelocity;
+
+        if (jumpInitialPush) // this should only be true for one frame / tick
+        {
+            jumpInitialPush = false;
+
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    
+    // editor feilds
+    [SerializeField]
+    private float playerSpeed = 10;
+    [SerializeField]
+    private float groundMovementSmoothing = 10; 
+    [SerializeField]
+    private float airMovementSmoothing = 10;
+
+    [SerializeField]
+    private GameObject debugPrefab;
+    [SerializeField]
+    LayerMask environment;
+
+    [SerializeField]
+    private float jumpVelocity = 20;
+    [SerializeField]
+    private float jumpCooldown = 0.1f;
+    private float timeWhenLastJumped = 0;
+    [SerializeField]
+    private float coyoteTime = 0.1f;
+    private float timeWhenCoyoteKickedIn = 0;
+
+    // other untiy bullshit
+    private GameObject player;
+    private Rigidbody2D playerRB;
+
+    // member variables
+    private Vector2 movementDirection;
+    private Bounds spriteBounds;
+
+    // flags for fixed update
+    bool canJump = false;
+    bool onFloor = false;
+    bool onFloorLastFrame = false;
+    bool beginJump = false;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+        player = gameObject;
+        playerRB = player.GetComponent<Rigidbody2D>();
+
+        spriteBounds = player.GetComponent<SpriteRenderer>().sprite.bounds;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+
+
+
+    }
+  
+    void handleIfCanJump()
+    {
+        
+        onFloorLastFrame = onFloor;
+        onFloor = Physics2D.Raycast(player.transform.position + spriteBounds.center - Vector3.right * spriteBounds.extents.x * 0.95f + Vector3.down * (spriteBounds.extents.y * 1.1f), Vector2.down, 0, environment)
+               || Physics2D.Raycast(player.transform.position + spriteBounds.center + Vector3.right * spriteBounds.extents.x * 0.95f + Vector3.down * (spriteBounds.extents.y * 1.1f), Vector2.down, 0, environment);
+
+        bool notInJumpCooldownn = (Time.time - timeWhenLastJumped > jumpCooldown);
+
+        if(onFloorLastFrame &&!onFloor) // coyote time kick in
+        {
+            timeWhenCoyoteKickedIn = Time.time;
+        }
+
+        bool pretendWeAreOnFloor = (Time.time - timeWhenCoyoteKickedIn < coyoteTime);
+
+        canJump = (onFloor || pretendWeAreOnFloor) && notInJumpCooldownn;
+
+
+    }
+
+    void HandleInput() 
+    {
+        movementDirection = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+
+        bool jumpInput = Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W); // replace this line with matt's call
+        if (jumpInput && canJump)
+        {
+            beginJump = true;
+            //canJump = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        handleIfCanJump();
+
+        Vector2 newVelocity = playerRB.velocity;
+
+        if (onFloor)
+        {
+            //Debug.Log("on floor");
+            if (Mathf.Abs(movementDirection.x) > 0) // moving
+            {
+                newVelocity.x = Mathf.Lerp(playerRB.velocity.x, movementDirection.x * playerSpeed, groundMovementSmoothing * Time.fixedDeltaTime);
+            }
+            else
+            {
+                newVelocity.x = Mathf.Lerp(playerRB.velocity.x, movementDirection.x * playerSpeed,2* groundMovementSmoothing * Time.fixedDeltaTime);
+
+            }
+            if (beginJump)
+            {
+                beginJump = false;
+                onFloor = false;
+                timeWhenLastJumped = Time.time;
+                newVelocity.y += jumpVelocity;
+            }
+        }
+        else // jumping or falling
+        {
+
+            if (movementDirection.x != 0)
+            {
+                //Debug.Log("movement direction not 0");
+                newVelocity.x = Mathf.Lerp(playerRB.velocity.x, movementDirection.x * playerSpeed, airMovementSmoothing * Time.fixedDeltaTime); 
+            }
+
+            Instantiate(debugPrefab, transform.position, Quaternion.identity);
+        }
+
+        playerRB.velocity = newVelocity;
+    }
+
+
+}
