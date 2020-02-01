7f580271fe90f74c1ba1d5342c0393d50e04ba32
Matt Stark <mattstark256@gmail.com>
2020-02-01 17:15:29 +0000

    added popups

---
+++ Assets/Popup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Popup : MonoBehaviour
+{
+    [SerializeField]
+    private Text text;
+
+    private PopupManager popupManager;
+
+    public void InitializePopup(PopupManager _popupManager)
+    {
+        popupManager = _popupManager;
+    }
+
+    public void SetText(string newText)
+    {
+        text.text = newText;
+    }
+
+    public void ClosePopup()
+    {
+        popupManager.CloseCurrentPopup();
+    }
+}
---
+++ Assets/PopupTester.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupTester : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            PopupManager.Instance.ShowPopup("test");
+        }
+    }
+}
