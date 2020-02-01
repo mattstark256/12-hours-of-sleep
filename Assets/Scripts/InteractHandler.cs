d0628c9abaf73a112393e72685f115e909a39b44
Scuba-steve-98 <47321591+Scuba-steve-98@users.noreply.github.com>
2020-02-01 18:18:01 +0000

    Door
    Door

---
+++ Assets/Scripts/InteractHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractHandler : MonoBehaviour
+{
+    GameObject currentObj = null;
+  //  public bool active = false;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.name);
+        if (other.CompareTag ("Interactable"))
+        {
+            currentObj = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Interactable"))
+        {
+            if (collision.gameObject == currentObj)
+            {
+                currentObj = null;
+            }
+        }
+    }
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetButtonDown("Interact") && currentObj)
+        //{
+        //    //open door
+        //    Debug.Log("pressed");
+        //    door.GetComponent<Door>().Open();
+        //    //currentObj.SendMessage("activate");
+        //}
+    }
+}
