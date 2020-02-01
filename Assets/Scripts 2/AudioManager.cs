16134df50c025485bfd0cd4d4546dfa67e900f23
JACOB BRETHERTON <1800231@uad.ac.uk>
2020-02-01 13:58:37 +0000

    Screen Shake added

---
+++ Assets/Scripts 2/AudioManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // Singleton
+    private static AudioManager instance;
+    public static AudioManager Instance { get { return instance; } }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts 2/CameraEffects.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraEffects : MonoBehaviour
+{
+
+    // Singleton
+    private static CameraEffects instance;
+    public static CameraEffects Instance { get { return instance; } }
+
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+
+
+    [SerializeField]
+    Camera camera;
+
+    ScreenShake screenShake;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenShake = camera.GetComponent<ScreenShake>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddScreenShake(float amount = 0.333f)
+    {
+        
+        screenShake.AddTrauma(amount);
+    }
+
+}
