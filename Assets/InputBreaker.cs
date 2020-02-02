4610097c76dea87c50cdcb1c4e21d436b22c3c55
Matt Stark <mattstark256@gmail.com>
2020-02-02 13:36:15 +0000

    added break popup

--- Assets/InputBreaker.cs
+++ Assets/InputBreaker.cs
@@ -7,24 +7,61 @@ public class InputBreaker : MonoBehaviour
     [SerializeField]
     private InputPanelController inputPanelController;
 
+    [SerializeField]
+    private BreakPopup breakPopupPrefab;
+    [SerializeField]
+    private Transform breakPopupParent;
+    [SerializeField]
+    private Collider2D invisibleBarrier;
+
+
     private bool broken = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!broken)
         {
-            AudioManager.Instance.Play("game_break");
-            inputPanelController.Break();
-            broken = true;
+            Break();
         }
     }
+
+
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.F2)&& Input.GetKeyDown(KeyCode.F5))
         if (!broken)
+            {
+                Break();
+            }
+    }
+
+
+    private void Break()
+    {
+        broken = true;
+        StartCoroutine(BreakCoroutine());
+
+    }
+
+    private IEnumerator BreakCoroutine()
+    {
+        BreakPopup breakPopup = Instantiate(breakPopupPrefab, breakPopupParent);
+
+        while (!breakPopup.buttonPressed)
         {
-            inputPanelController.Break();
-            broken = true;
+            yield return null;
         }
+
+        Destroy(breakPopup.gameObject);
+        AudioManager.Instance.Play("game_break");
+        inputPanelController.Break();
+        broken = true;
+
+        yield return new WaitForSeconds(2.5f);
+
+        Destroy(invisibleBarrier.gameObject);
+
+        PopupManager.Instance.ShowPopup("What have you done?!! You've broken the game! You better try to fix it.");
+        PopupManager.Instance.ShowPopup("The only key that's left is the A key. If you want to move right, you'll have to click and drag it into the move right slot.");
     }
 }
